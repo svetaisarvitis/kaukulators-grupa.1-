05945d1ee8ff4a2ac0d951d474374b94292487e6
svetaisarvitis <119396375+svetaisarvitis@users.noreply.github.com>
2023-04-04 08:41:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const plusins = document.querySelector('.js-plus');
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
+const answser = document.querySelector('.js-result');
+
+let firstValue = inputins1.value
+let secondValue = inputins2.value
+
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+const Subtract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+   console.log(Add(firstValue, secondValue))
+})
+
+minus.addEventListener('click', () => {
+})
+
+multiply.addEventListener('click', () => {
+})
+
+divide.addEventListener('click', () => {
+})
